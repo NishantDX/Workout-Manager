cc8736c761bcf6e434980bbd42e9229a6acb9ff3
Destructor693 <nishanty693@gmail.com>
2025-03-03 17:09:25 +0530

    iteration updated

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -7,9 +7,9 @@ import useWorkoutState from "./store/useworkoutState";
 import useWorkoutForMuscleState from "./store/useMuscleWorkoutState";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { getWorkout} =
-    useWorkoutState();
-  axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL; 
+  const { getWorkout } = useWorkoutState();
+  axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+
   useEffect(() => {
     const fetchWorkouts = async () => {
       try {
@@ -17,33 +17,39 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         if (!res) {
           throw new Error(`HTTP error! status: ${res}`);
         }
-        //console.log(res.data);
         getWorkout(res.data);
       } catch (error) {
         console.error("Error fetching workouts:", error);
       }
     };
     fetchWorkouts();
-  },[]);
+  }, []);
+
   const { muscleWorkouts, getMuscleWorkout } = useWorkoutForMuscleState();
+
   useEffect(() => {
     const fetchMuscleGroup = async () => {
       try {
         const res = await axios.get("/api/workouts");
-        //console.log(res);
         if (!res) {
           throw new Error(`HTTP error! status: ${res}`);
         }
 
-        getMuscleWorkout(res.data.muscleWiseWorkouts);
-        
+        // Check if res.data.muscleWiseWorkouts is iterable
+        if (Array.isArray(res.data.muscleWiseWorkouts)) {
+          getMuscleWorkout(res.data.muscleWiseWorkouts);
+        } else {
+          throw new Error("muscleWiseWorkouts is not an array");
+        }
       } catch (error) {
         console.error("Error fetching workouts:", error);
       }
     };
     fetchMuscleGroup();
   }, []);
+
   console.log(muscleWorkouts);
+
   return (
     <>
       <SideNav />
@@ -51,6 +57,3 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </>
   );
 }
-// function getMuscleWorkout(muscleWiseWorkouts: any) {
-//   throw new Error("Function not implemented.");
-// }
